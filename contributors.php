c55279bcb9c0fedb3debf9346b76503bb9be15bc
AyeZeeBB <nxmbx123@hotmail.com>
2023-10-16 12:42:50 -0500

    init

---
+++ contributors.php
@@ -0,0 +1,120 @@
+<?php
+$url = 'https://raw.githubusercontent.com/AyeZeeBB/R5R_Contributors/main/contributors.json';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$data = curl_exec($ch);
+curl_close($ch);
+
+$contributors = json_decode($data);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="./img/R5R-Logo-White.png">
+    <link rel="stylesheet" href="./css/home.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Contributors</title>
+    <meta content="R5Reloaded" property="og:title" />
+    <meta content="An Apex Legends Modding SDK" property="og:description" />
+    <meta content="https://r5reloaded.com" property="og:url" />
+    <meta content="https://r5reloaded.com/newsite/img/R5R-Logo-White.gif" property="og:image" />
+    <meta content="#d43d2c" data-react-helmet="true" name="theme-color" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="header">
+          <div class="logo">
+          <a href="./index.php"><img src="./img/R5R-Logo-White.gif" alt="logo" width="80" /></a>
+          </div>
+          <input type="checkbox" id="toggle" />
+          <div class="meniu servers details contribs">
+            <a class="btn" href="./index.php">Home</a>
+            <a class="btn" href="./servers.php">Servers</a>
+            <a class="btn" href="https://docs.r5reloaded.com/">Docs</a>
+            <a class="btn" href="./contributors.php">Contributors</a>
+            <a class="btn" href="./download.php">Download</a>
+          </div>
+          <label class="navbar-toggler" for="toggle">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+    
+    <main>
+      <div class="container" style="padding-top: 0px">
+        <div class="row">
+          <div class="background">
+            <div class="welcome" style="background-image: url('./img/contrib.png');height: 200px;">
+              <div class="description" style="width: 100%;justify-content: center;display: flex;flex-wrap: wrap;">
+                <p style="margin-bottom: 0px;margin-top: 0px;color: var(--white);">Contributors</p>
+              </div>
+            </div>
+            <div class="mainbox contribus">
+              <div class="black-background">
+                <br>
+                  <?php
+                    $number = 0;
+
+                    foreach($contributors->contributors as $data)
+                    {
+                      if($number == 0)
+                      {
+                        echo '<div class="block-content" style="margin-bottom: 0">';
+                      }
+                      
+                      if(empty($data->github))
+                        echo '<div class="block hover">';
+                      else
+                        echo '<a href="' . $data->github . '" class="block hover hover-animation">';
+
+                      echo '<div class="items">';
+                      echo '<img src="' . $data->image . '" alt="img" />';
+                      echo '<div class="item">';
+                      echo '<p style="font-size: 18px;"> ' . $data->name . '</p>';
+                      echo '<p class="text-white" style="margin-top: 0;font-size: 12px;">' . $data->desc . '</p>';
+                      
+                      if(empty($data->github)) {
+                        echo '<p class="text-white-name" style="margin-top: 0;font-size: 12px;">‎</p>';
+                      }
+                      else {
+                        echo '<p class="text-white-name" style="margin-top: 0;font-size: 12px;">Github: ' . explode("https://github.com/", $data->github)[1] . '</p>';
+                      }
+
+                      echo '</div>';
+                      echo '</div>';
+
+                      if(empty($data->github))
+                        echo '</div>';
+                      else
+                        echo '</a>';
+                      
+                      $number++;
+
+                      if($number > 3)
+                      {
+                        echo '</div>';
+                        $number = 0;
+                      }
+                    }
+                  ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/home.css" />
+    <link rel="icon" type="image/x-icon" href="./img/R5R-Logo-White.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>R5Reloaded</title>
+    <meta content="R5Reloaded" property="og:title" />
+    <meta content="An Apex Legends Modding SDK" property="og:description" />
+    <meta content="https://r5reloaded.com" property="og:url" />
+    <meta content="https://r5reloaded.com/newsite/img/R5R-Logo-White.gif" property="og:image" />
+    <meta content="#d43d2c" data-react-helmet="true" name="theme-color" />
+  </head>
+  <body>
+    <!-- header -->
+    <div class="container">
+      <div class="row">
+        <div class="header">
+          <div class="logo">
+            <a href="./index.php"><img src="./img/R5R-Logo-White.gif" alt="logo" width="80" /></a>
+          </div>
+          <input type="checkbox" id="toggle" />
+          <div class="meniu">
+            <a class="btn" href="./index.php">Home</a>
+            <a class="btn" href="./servers.php">Servers</a>
+            <a class="btn" href="https://docs.r5reloaded.com/">Docs</a>
+            <a class="btn" href="./contributors.php">Contributors</a>
+            <a class="btn" href="./download.php">Download</a>
+          </div>
+          <label class="navbar-toggler" for="toggle">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+    <main>
+      <div class="container" style="padding-top: 0px;">
+        <div class="row">
+          <div class="background">
+
+            <!-- Welcome -->
+            <div class="welcome" style="background-image: url('./img/banner.png');">
+              <div class="description" style="width:100%">
+                <p style="width:50%">Welcome To R5Reloaded</p>
+                <h1 style="width:50%">An Apex Legends Modding SDK</h1>
+                <div class="block-content2"  style="width:90%">
+                  <a href="https://github.com/Mauler125/r5sdk/releases/latest" class="button" style="margin-right: 10px;box-shadow: 0px 0px 10px #00000090;">Download</a>
+                  <a href="https://discord.gg/r5reloaded" class="discordbutton button-right" style="margin-right: 10px;box-shadow: 0px 0px 10px #00000090"><img src="./img/discord.png" alt="logo" width="25" /></a>
+                  <a href="https://github.com/Mauler125/r5sdk" class="githubbutton" style="margin-right: 10px;box-shadow: 0px 0px 10px #00000090;"><img src="./img/github.png" alt="logo" width="50" /></a>
+                  <a href="https://trello.com/b/ymr4R3j9/r5reloaded" class="trellobutton" style="box-shadow: 0px 0px 10px #00000090;"><img src="./img/trello.png" alt="logo" width="50" /></a>
+                </div>
+              </div>
+            </div>
+
+            <!-- Showcase -->
+            <div class="black-background" style="background-color: #1f212200;padding: 0px 0px 0px 0px;">
+              <section class="carrossel">
+                <div class="pic-ctn">
+                  <img src="./img/carrossel/1.jpg" alt="" class="pic">
+                  <img src="./img/carrossel/2.jpg" alt="" class="pic">
+                  <img src="./img/carrossel/3.jpg" alt="" class="pic">
+                  <img src="./img/carrossel/4.jpg" alt="" class="pic">
+                  <img src="./img/carrossel/5.jpg" alt="" class="pic">
+                </div> 
+              </section>
+            </div>
+
+
+            <!-- Whats R5Reloaded? -->
+            <div class="black-background">
+              <h3 class="title">Whats R5Reloaded?</h3>
+              <div class="block-content">
+                <div class="items">
+                  <div class="item">
+                    <div class="descriptionshowcase">
+                      <h1>Custom Servers</h1>
+                      <h2>Join or create fully custom servers.</h2>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="items">
+                  <div class="item">
+                    <div class="descriptionshowcase">
+                      <h1>Modding Support</h1>
+                      <h2>Modify your game or server however you like.</h2>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="items">
+                  <div class="item">
+                    <div class="descriptionshowcase">
+                      <h1>Private Matches</h1>
+                      <h2>Run around by your self or with friends.</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="block-content">
+                <div class="items">
+                  <div class="item">
+                    <div class="descriptionshowcase">
+                      <h1>Open Source</h1>
+                      <h2>Feel free to take a peak at what is going on behind the scenes.</h2>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="items">
+                  <div class="item">
+                    <div class="descriptionshowcase">
+                      <h1>Scripting</h1>
+                      <h2>Full access to the games scripting language.</h2>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="items">
+                  <div class="item">
+                    <div class="descriptionshowcase">
+                      <h1>Server List</h1>
+                      <h2>Easily join a variety of servers.</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
